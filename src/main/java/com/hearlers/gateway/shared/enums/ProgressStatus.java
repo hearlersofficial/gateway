723f2428f3f31e2c28fa662cb6fdcc48a41ecb3c
Devheun <dudwo5600@korea.ac.kr>
2024-12-04 00:47:38 +0900

    feat: proto enum들 shared 폴더로 이동 및 추가적인 enum 선언

--- src/main/java/com/hearlers/gateway/shared/enums/ProgressStatus.java
+++ src/main/java/com/hearlers/gateway/shared/enums/ProgressStatus.java
@@ -1,6 +1,7 @@
-package com.hearlers.gateway.presentations.rest.auth.enums;
+package com.hearlers.gateway.shared.enums;
 
 public enum ProgressStatus {
+    PROGRESS_STATUS_UNSPECIFIED,
     PROGRESS_STATUS_NOT_STARTED,
     PROGRESS_STATUS_IN_PROGRESS,
     PROGRESS_STATUS_COMPLETED,
