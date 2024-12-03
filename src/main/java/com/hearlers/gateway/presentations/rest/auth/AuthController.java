da0fde6d0f7cc3fc62a1c6448f4f38b2a0b34ee0
Devheun <dudwo5600@korea.ac.kr>
2024-12-04 02:31:36 +0900

    refactor: 비로그인 유저 생성 엔드포인트 수정 및 쿠키에 accessToken 만료기간도 추가

--- src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
+++ src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
@@ -8,7 +8,6 @@ import com.hearlers.gateway.applications.utils.service.UtilService;
 import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.CreateCookieRequestDto;
 import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.CreateCookieResponseDto;
 import com.hearlers.gateway.presentations.rest.auth.dto.CreateTokenRequestDto;
-import com.hearlers.gateway.presentations.rest.auth.dto.CreateUserRequestDto;
 import com.hearlers.gateway.shared.guard.dto.TokenDto;
 import com.hearlers.gateway.shared.guard.security.JwtUtil;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,8 +37,8 @@ public class AuthController {
     private static final int REFRESH_TOKEN_MAX_AGE = 60 * 60 * 24 * 7; // 7일
 
     // 비로그인 시 유저 생성
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody CreateUserRequestDto request, HttpServletResponse response) throws IOException {
+    @PostMapping("/initiate")
+    public void createUser(HttpServletResponse response) throws IOException {
         // TODO : request를 바탕으로 내부 서버와 통신하여 CreateUserResponseDto를 받아오기 (return 타입도 변경)
 
         // 이후 CreateUserResponseDto를 바탕으로 JWT 토큰 발급
@@ -50,6 +48,13 @@ public class AuthController {
 
         // 발급받은 accessToken 쿠키에 저장
         addCookieToResponse(response, token.getAccessToken(), "accessToken", ACCESS_TOKEN_MAX_AGE);
+        
+        String accessTokenExpiresAt = java.time.ZonedDateTime.now()
+                .plusSeconds(ACCESS_TOKEN_MAX_AGE)
+                .toInstant()
+                .toString();
+
+        addCookieToResponse(response, accessTokenExpiresAt, "accessTokenExpiresAt", ACCESS_TOKEN_MAX_AGE);
         response.sendRedirect("/");
     }
 
