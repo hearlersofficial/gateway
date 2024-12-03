0dbcef6389d0895b8ba2125c22560f09878d492c
Devheun <dudwo5600@korea.ac.kr>
2024-12-04 01:18:25 +0900

    refactor: AuthController 경로 수정

--- src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
+++ src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
@@ -1,7 +1,7 @@
 package com.hearlers.gateway.presentations.rest.auth;
 
-import static com.hearlers.gateway.presentations.rest.auth.enums.AuthChannel.AUTH_CHANNEL_KAKAO;
-import static com.hearlers.gateway.presentations.rest.auth.enums.AuthChannel.AUTH_CHANNEL_NONE;
+import static com.hearlers.gateway.shared.enums.AuthChannel.AUTH_CHANNEL_KAKAO;
+import static com.hearlers.gateway.shared.enums.AuthChannel.AUTH_CHANNEL_NONE;
 
 import com.hearlers.gateway.applications.auth.service.AuthService;
 import com.hearlers.gateway.applications.utils.service.UtilService;
