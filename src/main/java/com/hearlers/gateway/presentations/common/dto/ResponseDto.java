b89edebecd8fdce0aeb12e7c309936b6d5846efb
Devheun <dudwo5600@korea.ac.kr>
2024-12-06 02:57:10 +0900

    feat: 인증 부분 swagger 명세 추가

--- src/main/java/com/hearlers/gateway/presentations/common/dto/ResponseDto.java
+++ src/main/java/com/hearlers/gateway/presentations/common/dto/ResponseDto.java
@@ -1,28 +1,41 @@
 package com.hearlers.gateway.presentations.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
+import lombok.Getter;
 
 public class ResponseDto {
 
     @Builder
-    public static class Success {
+    @Getter
+    @Schema(description = "응답 성공 시 반환되는 DTO")
+    public static class Success<D> {
         @Builder.Default
+        @Schema(description = "응답 상태", example = "SUCCESS")
         private String status = "SUCCESS";
 
+        @Schema(description = "메시지")
         private String message;
 
-        private Object data;
+        @Schema(description = "데이터")
+        private D data;
     }
 
     @Builder
-    public static class Error {
+    @Getter
+    @Schema(description = "응답 실패 시 반환되는 DTO")
+    public static class Error<D> {
         @Builder.Default
+        @Schema(description = "응답 상태", example = "ERROR")
         private String status = "ERROR";
 
+        @Schema(description = "에러 코드")
         private String code;
 
+        @Schema(description = "메시지")
         private String message;
 
-        private Object data;
+        @Schema(description = "데이터")
+        private D data;
     }
 }
--- src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
+++ src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
@@ -7,13 +7,22 @@ import com.hearlers.gateway.applications.auth.service.AuthService;
 import com.hearlers.gateway.applications.utils.service.UtilService;
 import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.CreateCookieRequestDto;
 import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.CreateCookieResponseDto;
+import com.hearlers.gateway.presentations.common.dto.ResponseDto;
 import com.hearlers.gateway.presentations.rest.auth.dto.CreateTokenRequestDto;
 import com.hearlers.gateway.shared.guard.dto.TokenDto;
 import com.hearlers.gateway.shared.guard.security.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "AuthController", description = "로그인, 회원가입, 토큰 발급 등 인증 관련 API")
 public class AuthController {
     private final AuthService authService;
     private final JwtUtil jwtUtil;
@@ -36,9 +46,14 @@ public class AuthController {
     private static final int ACCESS_TOKEN_MAX_AGE = 60 * 60; // 1시간
     private static final int REFRESH_TOKEN_MAX_AGE = 60 * 60 * 24 * 7; // 7일
 
-    // 비로그인 시 유저 생성
+
+    @Operation(summary = "비로그인 유저 생성", description = "비로그인 유저를 생성하고, accessToken 발급")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "비로그인 유저 생성 성공, 쿠키에 accessToken과 accessTokenExpiresAt 저장", content = @Content(schema = @Schema(implementation = ResponseDto.Success.class))),
+            @ApiResponse(responseCode = "400", description = "비로그인 유저 생성 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
+    })
     @PostMapping("/initiate")
-    public void createUser(HttpServletResponse response) throws IOException {
+    public ResponseEntity<ResponseDto.Success<TokenDto>> createUser(HttpServletResponse response) {
         // TODO : request를 바탕으로 내부 서버와 통신하여 CreateUserResponseDto를 받아오기 (return 타입도 변경)
 
         // 이후 CreateUserResponseDto를 바탕으로 JWT 토큰 발급
@@ -55,9 +70,21 @@ public class AuthController {
                 .toString();
 
         addCookieToResponse(response, accessTokenExpiresAt, "accessTokenExpiresAt", ACCESS_TOKEN_MAX_AGE);
-        response.sendRedirect("/");
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(
+                        ResponseDto.Success.<TokenDto>builder()
+                                .message("비로그인 유저 생성 성공")
+                                .data(token)
+                                .build()
+                );
     }
 
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인을 위한 인증 코드 요청, 카카오로 리다이렉트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "302", description = "카카오로 리다이렉트"),
+            @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
+    })
     @GetMapping("/login/kakao")
     public void kakao(HttpServletResponse response) throws IOException {
         StringBuffer url = new StringBuffer();
@@ -68,6 +95,11 @@ public class AuthController {
         response.sendRedirect(url.toString());
     }
 
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인을 통해 받은 인증코드 바탕으로 액세스토큰과 리프레시토큰 발급 후 쿠키에 저장")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "302", description = "카카오 로그인 유저 생성 성공, 쿠키에 accessToken과 accessTokenExpiresAt, refreshToken과 refreshTokenExpiresAt 저장"),
+            @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
+    })
     @GetMapping("/callback/kakao")
     public void kakaoCallback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         authService.kakaoLogin(code);
@@ -101,6 +133,12 @@ public class AuthController {
         response.sendRedirect("/");
     }
 
+
+    @Operation(summary = "액세스 토큰 재발급", description = "리프레시 토큰으로 액세스 토큰 재발급")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "액세스 토큰 재발급 성공, 쿠키에 accessToken과 accessTokenExpiresAt 저장", content = @Content(schema = @Schema(implementation = ResponseDto.Success.class))),
+            @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
+    })
     @PostMapping("/refresh")
     public void refreshToken() {
         // TODO : refreshToken을 바탕으로 새로운 accessToken 발급
