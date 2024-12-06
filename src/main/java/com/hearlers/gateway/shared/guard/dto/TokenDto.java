19eab453608e8afe38fcbd90591216229efae8d4
Devheun <dudwo5600@korea.ac.kr>
2024-12-06 19:19:53 +0900

    feat: 토큰 DTO Swagger 명세 적용

--- src/main/java/com/hearlers/gateway/shared/guard/dto/TokenDto.java
+++ src/main/java/com/hearlers/gateway/shared/guard/dto/TokenDto.java
@@ -3,13 +3,18 @@ package com.hearlers.gateway.shared.guard.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 @Builder
+@Schema(name = "TokenDto", description = "액세스 토큰과 리프레시 토큰을 담은 DTO")
 public class TokenDto {
+    @Schema(description = "액세스 토큰")
     private String accessToken;
+
+    @Schema(description = "리프레시 토큰")
     private String refreshToken;
 }
