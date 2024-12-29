4032a4dae52ed0c836304ef1ef8f55b06abd9df0
Devheun <86945971+Devheun@users.noreply.github.com>
2024-12-30 01:40:21 +0900

    feat: interceptor 적용 및 kakao login api에 state param 추가

--- src/main/java/com/hearlers/gateway/config/WebConfig.java
+++ src/main/java/com/hearlers/gateway/config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.hearlers.gateway.config;
 
+import com.hearlers.gateway.shared.guard.security.AuthGuardInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final AuthGuardInterceptor authGuardInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -17,5 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:8080"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authGuardInterceptor)
+                .addPathPatterns("/**"); // 모든 경로에 적용
     }
 }
--- src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
+++ src/main/java/com/hearlers/gateway/presentations/rest/auth/AuthController.java
@@ -1,8 +1,12 @@
 package com.hearlers.gateway.presentations.rest.auth;
 
-import static com.hearlers.gateway.shared.enums.AuthChannel.AUTH_CHANNEL_KAKAO;
-import static com.hearlers.gateway.shared.enums.AuthChannel.AUTH_CHANNEL_NONE;
-
+import com.hearlers.api.proto.v1.model.AuthChannel;
+import com.hearlers.api.proto.v1.model.AuthUser;
+import com.hearlers.api.proto.v1.service.InitializeUserRequest;
+import com.hearlers.api.proto.v1.service.InitializeUserResponse;
+import com.hearlers.api.proto.v1.service.SaveRefreshTokenRequest;
+import com.hearlers.api.proto.v1.service.SaveRefreshTokenResponse;
+import com.hearlers.api.proto.v1.service.UserServiceGrpc;
 import com.hearlers.gateway.applications.auth.service.AuthService;
 import com.hearlers.gateway.applications.utils.service.UtilService;
 import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.CreateCookieRequestDto;
@@ -10,6 +14,7 @@ import com.hearlers.gateway.applications.utils.useCases.CreateCookieUseCase.dto.
 import com.hearlers.gateway.presentations.common.dto.ResponseDto;
 import com.hearlers.gateway.presentations.rest.auth.dto.CreateTokenRequestDto;
 import com.hearlers.gateway.shared.guard.dto.TokenDto;
+import com.hearlers.gateway.shared.guard.security.AuthGuard;
 import com.hearlers.gateway.shared.guard.security.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -17,6 +22,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +31,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +44,8 @@ public class AuthController {
     private final AuthService authService;
     private final JwtUtil jwtUtil;
     private final UtilService utilService;
+    private final UserServiceGrpc.UserServiceBlockingStub userServiceBlockingStub;
+
 
     @Value("${kakao.client_id}")
     String clientId;
@@ -54,11 +63,12 @@ public class AuthController {
     })
     @PostMapping("/initiate")
     public ResponseEntity<ResponseDto.Success<TokenDto>> createUser(HttpServletResponse response) {
-        // TODO : request를 바탕으로 내부 서버와 통신하여 CreateUserResponseDto를 받아오기 (return 타입도 변경)
+        InitializeUserRequest request = InitializeUserRequest.newBuilder().build();
+        InitializeUserResponse initializeUserResponse = userServiceBlockingStub.initializeUser(request);
 
-        // 이후 CreateUserResponseDto를 바탕으로 JWT 토큰 발급
-        // id 1로 임의로 설정
-        CreateTokenRequestDto dto = new CreateTokenRequestDto(1, AUTH_CHANNEL_NONE);
+        CreateTokenRequestDto dto = new CreateTokenRequestDto(initializeUserResponse.getUser().getId(),
+                initializeUserResponse.getAuthUser().getAuthChannel());
+
         TokenDto token = jwtUtil.returnToken(dto, false);
 
         // 발급받은 accessToken 쿠키에 저장
@@ -86,12 +96,14 @@ public class AuthController {
             @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
     })
     @GetMapping("/login/kakao")
-    public void kakao(HttpServletResponse response) throws IOException {
+    @AuthGuard
+    public void kakao(@RequestAttribute("userId") int userId, HttpServletResponse response) throws IOException {
         StringBuffer url = new StringBuffer();
         url.append("https://kauth.kakao.com/oauth/authorize?");
         url.append("client_id=").append(clientId);
         url.append("&redirect_uri=").append(redirectUri);
         url.append("&response_type=code");
+        url.append("&state=").append(userId);
         response.sendRedirect(url.toString());
     }
 
@@ -101,15 +113,26 @@ public class AuthController {
             @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
     })
     @GetMapping("/callback/kakao")
-    public void kakaoCallback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
-        authService.kakaoLogin(code);
+    public void kakaoCallback(@RequestParam("code") String code, @RequestParam("state") String state,
+                              HttpServletResponse response) throws IOException {
 
-        /*
-        TODO : 유저 refreshToken 저장해야하니 UserService에 요청 날리기
-        * */
+        int userId = Integer.parseInt(state);
+        AuthUser authUser = authService.kakaoLogin(code, userId);
+        AuthChannel authChannel = authUser.getAuthChannel();
+        
+        TokenDto token = jwtUtil.returnToken(new CreateTokenRequestDto(userId, authChannel), true);
+        SaveRefreshTokenRequest saveRefreshTokenRequest = SaveRefreshTokenRequest.newBuilder()
+                .setUserId(userId)
+                .setToken(token.getRefreshToken())
+                .setExpiresAt(token.getRefreshTokenExpiresAt().toString())
+                .build();
 
-        // userId 임의로 1로 설정 (accessToken과 refreshToken 발급)
-        TokenDto token = jwtUtil.returnToken(new CreateTokenRequestDto(1, AUTH_CHANNEL_KAKAO), true);
+        SaveRefreshTokenResponse saveRefreshTokenResponse = userServiceBlockingStub.saveRefreshToken(
+                saveRefreshTokenRequest);
+
+        if (!saveRefreshTokenResponse.getSuccess()) {
+            throw new Error("RefreshToken 저장 실패");
+        }
 
         // 발급받은 accessToken 쿠키에 저장
         addCookieToResponse(response, token.getAccessToken(), "accessToken", ACCESS_TOKEN_MAX_AGE);
@@ -140,7 +163,8 @@ public class AuthController {
             @ApiResponse(responseCode = "400", description = "카카오 로그인 실패", content = @Content(schema = @Schema(implementation = ResponseDto.Error.class)))
     })
     @PostMapping("/refresh")
-    public void refreshToken() {
+    @AuthGuard
+    public void refreshToken(HttpServletRequest request) {
         // TODO : refreshToken을 바탕으로 새로운 accessToken 발급
     }
 
