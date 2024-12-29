8d36180d2982a39e0ebcc698607ae65fbfacb778
Devheun <86945971+Devheun@users.noreply.github.com>
2024-12-30 01:37:58 +0900

    feat: Jwt 검증 및 AuthGuard를 통한 userId 추출

--- src/main/java/com/hearlers/gateway/shared/guard/security/JwtUtil.java
+++ src/main/java/com/hearlers/gateway/shared/guard/security/JwtUtil.java
@@ -3,17 +3,22 @@ package com.hearlers.gateway.shared.guard.security;
 import com.hearlers.gateway.presentations.rest.auth.dto.CreateTokenRequestDto;
 import com.hearlers.gateway.shared.guard.dto.TokenDto;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.time.ZonedDateTime;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class JwtUtil {
     private final Key key;
     private final long accessTokenExpTime;
@@ -29,7 +34,7 @@ public class JwtUtil {
         this.accessTokenExpTime = accessTokenExpTime;
         this.refreshTokenExpTime = refreshTokenExpTime;
     }
-    
+
     // accessToken 생성 및 반환
     public TokenDto returnToken(CreateTokenRequestDto dto, boolean rememberMe) {
         return createToken(dto, accessTokenExpTime, refreshTokenExpTime, rememberMe);
@@ -57,6 +62,7 @@ public class JwtUtil {
         if (!rememberMe) {
             return TokenDto.builder()
                     .accessToken(accessToken)
+                    .accessTokenExpiresAt(accessTokenValidity.toLocalDateTime())
                     .build();
         } else {
             // refresh token
@@ -69,8 +75,61 @@ public class JwtUtil {
 
             return TokenDto.builder()
                     .accessToken(accessToken)
+                    .accessTokenExpiresAt(accessTokenValidity.toLocalDateTime())
                     .refreshToken(refreshToken)
+                    .refreshTokenExpiresAt(refreshTokenValidity.toLocalDateTime())
                     .build();
         }
+    }
+
+    // JWT claim 추출
+    public Claims parseClaims(String token) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+
+    // token에서 user_id 추출
+    public int getUserId(String token) {
+        return parseClaims(token).get("id", Integer.class);
+    }
+
+    // JWT가 만료되었는지 확인하는 메서드
+    public boolean isTokenExpired(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+            Date expiration = claims.getExpiration();
+            return expiration.before(new Date());
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return true;
+    }
+
+    // JWT 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+
+        return false;
     }
 }
