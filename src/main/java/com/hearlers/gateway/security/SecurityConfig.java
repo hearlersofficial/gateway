fa16350bb4eb55ac204a5c889ccf0daed0ff5564
Devheun <86945971+Devheun@users.noreply.github.com>
2025-02-27 20:13:07 +0900

    feat: 스프링 시큐리티 폼 로그인 비활성화

--- src/main/java/com/hearlers/gateway/security/SecurityConfig.java
+++ src/main/java/com/hearlers/gateway/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,7 +24,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtUtil jwtUtil) throws Exception {
         httpSecurity
                 .httpBasic(HttpBasicConfigurer::disable) // HTTP 기본 인증 비활성화
-                .csrf(CsrfConfigurer::disable); // CSRF 보호 비활성화
+                .csrf(CsrfConfigurer::disable) // CSRF 보호 비활성화
+                .formLogin(AbstractHttpConfigurer::disable); // 폼 로그인 비활성화
 
         // 경로별 권한 설정
         httpSecurity
