ce58dd58278bb07281b27a22dc7922f14eaa2b7f
do-huni <orangnlp@gmail.com>
2025-03-13 01:43:00 +0900

    feat: instruction api

--- src/main/java/com/hearlers/gateway/application/counseling/PromptsPersistor.java
+++ src/main/java/com/hearlers/gateway/application/counseling/PromptsPersistor.java
@@ -1,13 +1,16 @@
 package com.hearlers.gateway.application.counseling;
 
 import com.hearlers.api.proto.v1.model.Context;
+import com.hearlers.api.proto.v1.model.Instruction;
 import com.hearlers.api.proto.v1.model.InstructionItem;
 import com.hearlers.api.proto.v1.model.Tone;
 import com.hearlers.api.proto.v1.service.CreateContextRequest;
 import com.hearlers.api.proto.v1.service.CreateInstructionItemRequest;
+import com.hearlers.api.proto.v1.service.CreateInstructionRequest;
 import com.hearlers.api.proto.v1.service.CreateToneRequest;
 import com.hearlers.api.proto.v1.service.UpdateContextRequest;
 import com.hearlers.api.proto.v1.service.UpdateInstructionItemRequest;
+import com.hearlers.api.proto.v1.service.UpdateInstructionRequest;
 import com.hearlers.api.proto.v1.service.UpdateToneRequest;
 
 public interface PromptsPersistor {
@@ -17,4 +20,6 @@ public interface PromptsPersistor {
     Tone updateTone(UpdateToneRequest request);
     InstructionItem createInstructionItem(CreateInstructionItemRequest request);
     InstructionItem updateInstructionItem(UpdateInstructionItemRequest request);
+    Instruction createInstruction(CreateInstructionRequest request);
+    Instruction updateInstruction(UpdateInstructionRequest request);
 }
--- src/main/java/com/hearlers/gateway/application/counseling/PromptsReader.java
+++ src/main/java/com/hearlers/gateway/application/counseling/PromptsReader.java
@@ -3,12 +3,15 @@ package com.hearlers.gateway.application.counseling;
 import java.util.List;
 
 import com.hearlers.api.proto.v1.model.Context;
+import com.hearlers.api.proto.v1.model.Instruction;
 import com.hearlers.api.proto.v1.model.InstructionItem;
 import com.hearlers.api.proto.v1.model.Tone;
 import com.hearlers.api.proto.v1.service.FindContextByIdRequest;
 import com.hearlers.api.proto.v1.service.FindContextsRequest;
+import com.hearlers.api.proto.v1.service.FindInstructionByIdRequest;
 import com.hearlers.api.proto.v1.service.FindInstructionItemByIdRequest;
 import com.hearlers.api.proto.v1.service.FindInstructionItemsRequest;
+import com.hearlers.api.proto.v1.service.FindInstructionsRequest;
 import com.hearlers.api.proto.v1.service.FindToneByIdRequest;
 import com.hearlers.api.proto.v1.service.FindTonesRequest;
 
@@ -19,4 +22,6 @@ public interface PromptsReader {
     List<Tone> findTones(FindTonesRequest request);
     InstructionItem findInstructionItemById(FindInstructionItemByIdRequest request);
     List<InstructionItem> findInstructionItems(FindInstructionItemsRequest request);
+    Instruction findInstructionById(FindInstructionByIdRequest request);
+    List<Instruction> findInstructions(FindInstructionsRequest request);
 }
