997e468bd8af6402d77201086e03703b690a383e
do-huni <orangnlp@gmail.com>
2025-03-13 01:21:06 +0900

    feat: tone api

--- src/main/java/com/hearlers/gateway/application/counseling/CounselingServiceImpl.java
+++ src/main/java/com/hearlers/gateway/application/counseling/CounselingServiceImpl.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.hearlers.api.proto.v1.model.Context;
+import com.hearlers.api.proto.v1.model.Tone;
 import com.hearlers.api.proto.v1.service.CreateContextRequest;
+import com.hearlers.api.proto.v1.service.CreateToneRequest;
 import com.hearlers.api.proto.v1.service.FindContextByIdRequest;
 import com.hearlers.api.proto.v1.service.FindContextsRequest;
+import com.hearlers.api.proto.v1.service.FindToneByIdRequest;
+import com.hearlers.api.proto.v1.service.FindTonesRequest;
 import com.hearlers.api.proto.v1.service.UpdateContextRequest;
+import com.hearlers.api.proto.v1.service.UpdateToneRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +22,28 @@ import lombok.RequiredArgsConstructor;
 public class CounselingServiceImpl implements CounselingService {
     private final PromptsPersistor persistor;
     private final PromptsReader reader;
+
+    
+
+    @Override
+    public Tone createTone(CreateToneRequest request) {
+        return persistor.createTone(request);
+    }
+
+    @Override
+    public Tone updateTone(UpdateToneRequest request) {
+        return persistor.updateTone(request);
+    }
+
+    @Override
+    public Tone findToneById(FindToneByIdRequest request) {
+        return reader.findToneById(request);
+    }
+
+    @Override
+    public List<Tone> findTones(FindTonesRequest request) {
+        return reader.findTones(request);
+    }
 
     @Override
     public Context createContext(CreateContextRequest request) {
@@ -32,7 +59,6 @@ public class CounselingServiceImpl implements CounselingService {
     public Context findContextById(FindContextByIdRequest request) {
         return reader.findContextById(request);
     }
-    
 
     @Override
     public List<Context> findContexts(FindContextsRequest request) {
