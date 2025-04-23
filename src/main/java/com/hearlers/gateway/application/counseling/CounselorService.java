3984ca17660e6ab436f736aa60ac1b44c4a58f5d
do-huni <orangnlp@gmail.com>
2025-04-23 21:07:25 +0900

    feat: 상담사 조회 메서드 구현

--- src/main/java/com/hearlers/gateway/application/counseling/CounselorService.java
+++ src/main/java/com/hearlers/gateway/application/counseling/CounselorService.java
@@ -1,13 +1,20 @@
 package com.hearlers.gateway.application.counseling;
 
-import com.hearlers.api.proto.v1.model.Counsel;
-import com.hearlers.api.proto.v1.model.Counselor;
-import com.hearlers.api.proto.v1.model.*;
-import com.hearlers.api.proto.v1.service.*;
-
 import java.util.List;
+
+import com.hearlers.api.proto.v1.model.Counselor;
+import com.hearlers.api.proto.v1.model.Tone;
+import com.hearlers.api.proto.v1.service.FindCounselorByIdRequest;
+import com.hearlers.api.proto.v1.service.FindCounselorsRequest;
+import com.hearlers.api.proto.v1.service.FindToneByIdRequest;
+import com.hearlers.api.proto.v1.service.FindTonesRequest;
 
 public interface CounselorService {
     // Counselor
+    Counselor findCounselorById(FindCounselorByIdRequest request);
     List<Counselor> findCounselors(FindCounselorsRequest request);
+
+    // Tone
+    Tone findToneById(FindToneByIdRequest request);
+    List<Tone> findTones(FindTonesRequest request);
 }
--- src/main/java/com/hearlers/gateway/application/counseling/CounselorServiceImpl.java
+++ src/main/java/com/hearlers/gateway/application/counseling/CounselorServiceImpl.java
@@ -1,20 +1,43 @@
 package com.hearlers.gateway.application.counseling;
 
 import com.hearlers.api.proto.v1.model.Counselor;
+import com.hearlers.api.proto.v1.service.FindCounselorByIdRequest;
 import com.hearlers.api.proto.v1.service.FindCounselorsRequest;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import com.hearlers.api.proto.v1.model.Tone;
+import com.hearlers.api.proto.v1.service.FindToneByIdRequest;
+import com.hearlers.api.proto.v1.service.FindTonesRequest;
 
 @Service
 @RequiredArgsConstructor
 public class CounselorServiceImpl implements CounselorService {
     private final CounselorReader counselorReader;
 
+
+    @Override
+    public Counselor findCounselorById(FindCounselorByIdRequest request) {
+        return counselorReader.findCounselorById(request);
+    }
+
     @Override
     public List<Counselor> findCounselors(FindCounselorsRequest request) {
         return counselorReader.findCounselors(request);
     }
 
+    @Override
+    public Tone findToneById(FindToneByIdRequest request) {
+        return counselorReader.findToneById(request);
+    }
+
+    @Override
+    public List<Tone> findTones(FindTonesRequest request) {
+        return counselorReader.findTones(request);
+    }
+    
 }
