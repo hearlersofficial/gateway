17fae1498ecd99687398832f127d1f11c4298bff
astor-dev <orangnlp@gmail.com>
2025-09-01 16:51:12 +0900

    build: usecase 모듈 init

---
+++ usecase/core/src/main/java/com/hearlers/gateway/port/TokenManagerPort.java
@@ -0,0 +1,17 @@
+package com.hearlers.gateway.port;
+
+
+import com.hearlers.api.proto.v1.model.Authority;
+import com.hearlers.api.proto.v1.service.SaveRefreshTokenRequest;
+import com.hearlers.api.proto.v1.service.SaveRefreshTokenResponse;
+import com.hearlers.api.proto.v1.service.VerifyRefreshTokenRequest;
+import com.hearlers.api.proto.v1.service.VerifyRefreshTokenResponse;
+
+
+public interface TokenManagerPort {
+//    AuthInfo.TokenInfo generateToken(AuthCommand.GenerateTokenCommand command, boolean withRefreshToken, Authority authority);
+    Authority getAuthorityFromToken(String token);
+    boolean validateToken(String token);
+    SaveRefreshTokenResponse saveRefreshToken(SaveRefreshTokenRequest request);
+    VerifyRefreshTokenResponse verifyRefreshToken(VerifyRefreshTokenRequest request);
+}
