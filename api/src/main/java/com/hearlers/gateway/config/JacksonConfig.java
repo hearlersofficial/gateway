baea4c6e983a3a0567d592a7a9145c92cf842754
astor-dev <orangnlp@gmail.com>
2025-09-08 19:54:38 +0900

    chore: proto 동기화

--- api/src/main/java/com/hearlers/gateway/config/JacksonConfig.java
+++ api/src/main/java/com/hearlers/gateway/config/JacksonConfig.java
@@ -2,12 +2,9 @@ package com.hearlers.gateway.config;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hearlers.gateway.DefaultObjectMapper;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JacksonConfig {
