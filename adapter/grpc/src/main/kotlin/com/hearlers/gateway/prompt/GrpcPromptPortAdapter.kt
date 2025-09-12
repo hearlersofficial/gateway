7a097e9c6111e5146308f65b2b6b4602a2198557
astor-dev <orangnlp@gmail.com>
2025-09-12 20:51:24 +0900

    feat: **-prompt 코루틴 stub 사용

--- adapter/grpc/src/main/kotlin/com/hearlers/gateway/prompt/GrpcPromptPortAdapter.kt
+++ adapter/grpc/src/main/kotlin/com/hearlers/gateway/prompt/GrpcPromptPortAdapter.kt
@@ -2,7 +2,7 @@ package com.hearlers.gateway.prompt
 
 import com.hearlers.api.proto.v1.model.PersonaPrompt
 import com.hearlers.api.proto.v1.model.TonePrompt
-import com.hearlers.api.proto.v1.service.CounselPromptServiceGrpc
+import com.hearlers.api.proto.v1.service.CounselPromptServiceGrpcKt
 import com.hearlers.api.proto.v1.service.FindPersonaPromptByIdRequest
 import com.hearlers.api.proto.v1.service.FindPersonaPromptsRequest
 import com.hearlers.api.proto.v1.service.FindTonePromptByIdRequest
@@ -14,30 +14,30 @@ import org.springframework.stereotype.Component
 
 @Component
 class GrpcPromptPortAdapter(
-    private val counselPromptServiceBlockingStub: CounselPromptServiceGrpc.CounselPromptServiceBlockingStub
+    private val counselPromptServiceCoroutineStub: CounselPromptServiceGrpcKt.CounselPromptServiceCoroutineStub
 
 ) : PromptPort {
-    override fun updateTonePrompt(request: UpdateTonePromptRequest): TonePrompt {
-        return this.counselPromptServiceBlockingStub.updateTonePrompt(request).tonePrompt
+    override suspend fun updateTonePrompt(request: UpdateTonePromptRequest): TonePrompt {
+        return this.counselPromptServiceCoroutineStub.updateTonePrompt(request).tonePrompt
     }
 
-    override fun updatePersonaPrompt(request: UpdatePersonaPromptRequest): PersonaPrompt {
-        return this.counselPromptServiceBlockingStub.updatePersonaPrompt(request).personaPrompt
+    override suspend fun updatePersonaPrompt(request: UpdatePersonaPromptRequest): PersonaPrompt {
+        return this.counselPromptServiceCoroutineStub.updatePersonaPrompt(request).personaPrompt
     }
 
-    override fun findTonePromptById(request: FindTonePromptByIdRequest): TonePrompt? {
-        return this.counselPromptServiceBlockingStub.findTonePromptById(request).tonePrompt
+    override suspend fun findTonePromptById(request: FindTonePromptByIdRequest): TonePrompt? {
+        return this.counselPromptServiceCoroutineStub.findTonePromptById(request).tonePrompt
     }
 
-    override fun findTonePrompts(request: FindTonePromptsRequest): List<TonePrompt> {
-        return this.counselPromptServiceBlockingStub.findTonePrompts(request).tonePromptsList
+    override suspend fun findTonePrompts(request: FindTonePromptsRequest): List<TonePrompt> {
+        return this.counselPromptServiceCoroutineStub.findTonePrompts(request).tonePromptsList
     }
 
-    override fun findPersonaPromptById(request: FindPersonaPromptByIdRequest): PersonaPrompt? {
-        return this.counselPromptServiceBlockingStub.findPersonaPromptById(request).personaPrompt
+    override suspend fun findPersonaPromptById(request: FindPersonaPromptByIdRequest): PersonaPrompt? {
+        return this.counselPromptServiceCoroutineStub.findPersonaPromptById(request).personaPrompt
     }
 
-    override fun findPersonaPrompts(request: FindPersonaPromptsRequest): List<PersonaPrompt> {
-        return this.counselPromptServiceBlockingStub.findPersonaPrompts(request).personaPromptsList
+    override suspend fun findPersonaPrompts(request: FindPersonaPromptsRequest): List<PersonaPrompt> {
+        return this.counselPromptServiceCoroutineStub.findPersonaPrompts(request).personaPromptsList
     }
 }
