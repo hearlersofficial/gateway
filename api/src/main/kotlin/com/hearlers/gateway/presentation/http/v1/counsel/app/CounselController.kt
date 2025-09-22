fef5b5453d537b36725f92251c2a58b5d5bd0397
Devheun <86945971+Devheun@users.noreply.github.com>
2025-09-22 21:31:48 +0900

    refactor: proto 파일 변경에 따른 로직 대응

--- api/src/main/kotlin/com/hearlers/gateway/presentation/http/v1/counsel/app/CounselController.kt
+++ api/src/main/kotlin/com/hearlers/gateway/presentation/http/v1/counsel/app/CounselController.kt
@@ -169,10 +169,9 @@ class CounselController(
     )
     @GetMapping("/counselor-user-relationships")
     fun getCounselorUserRelationships(
-        @RequestParam(required = false) counselorId: String?,
         @RequestAttribute(value = "userId") userId: String
     ): ResponseEntity<ResponseDto.Success<CounselDto.FindCounselorUserRelationshipsResponse>> {
-        val findRelationshipsRequest = CounselDtoMapper.toFindCounselorUserRelationshipsRequest(counselorId, userId)
+        val findRelationshipsRequest = CounselDtoMapper.toFindCounselorUserRelationshipsRequest(userId)
         val relationships = runBlocking { counselUseCase.findCounselorUserRelationships(findRelationshipsRequest) }
         val response = CounselDtoMapper.toFindCounselorUserRelationshipsResponse(relationships)
         return ResponseDtoUtil.okResponse(response, "관계 조회 성공")
